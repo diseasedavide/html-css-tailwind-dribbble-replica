86e714bada2675180a0ec663e815c9b3d361d8a9
Davide <rinna.davide@gmail.com>
2024-02-05 11:30:55 +0100

    aggiunto logo su navbar, creata struttura pagina principale e abbozzato ul

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   theme: {
+//     extend: {
+//       keyframes: {
+//         sfondo: {
+//           "0%": { transform: "bg-red-500" },
+//           "50%": { transform: "bg-purple-500" },
+//         },
+//       },
+//     },
+//   },
+// };
